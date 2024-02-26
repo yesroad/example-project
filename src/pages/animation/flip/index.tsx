6b733e06aad9e187f28df0cf25e3abd140b5ea76
김응길 <yesroad.dev@gmail.com>
2024-02-26 20:55:13 +0900

    주석 추가

--- src/pages/animation/flip/index.tsx
+++ src/pages/animation/flip/index.tsx
@@ -3,10 +3,19 @@ import styles from './flip.module.scss';
 import classNames from 'classnames';
 
 function Flip() {
+	/**
+	 * UP, DOWN, NONE 3가지 타입으로 지정 (기본값 NONE)
+	 * 기본값은 서버에서 값 내려오는걸로처리해줄거임
+	 * up - 올라감
+	 * down - 내려감
+	 * NONE - 선택안함
+	 */
+
 	const [flipAni, setFlipAni] = useState<'UP' | 'DOWN' | 'NONE'>('NONE');
 
 	return (
 		<div className={styles.wrapper}>
+			{/* 기본 카드 버튼 */}
 			<div
 				className={classNames(styles.defaultWrapper, {
 					[styles.fadeOutAniActive]: flipAni !== 'NONE',
@@ -19,6 +28,7 @@ function Flip() {
 					DOWN 기본 배경
 				</div>
 			</div>
+			{/* flip 애니메이션 */}
 			<div className={styles.flipBox}>
 				{flipAni === 'UP' && (
 					<div className={classNames([styles.flip, styles.flipAniActive])}>
@@ -33,6 +43,8 @@ function Flip() {
 					</div>
 				)}
 			</div>
+
+			{/* 다시하기 버튼 */}
 			<button className={styles.button} onClick={() => setFlipAni('NONE')}>
 				기본 상태로 리셋
 			</button>
